4bb88ada8ba1ed26d16333bdfeb49c2fef02796e
Christos Evangelides <christopher.evangelides@gmail.com>
2019-03-06 04:49:38 -0500

    updates the testAJAX for the first time

---
+++ testAJAX.js
@@ -0,0 +1,18 @@
+var xhr; //define global variable to process AJAX request
+
+function callDivChange(){
+	xhr = new XMLHttpRequest();
+	var filename = document.getElementById("filename").value; //get content in filename text box
+	xhr.open("GET", filename, true); // send to server
+	xhr.onreadystatechange = processDivChange;
+	xhr.send();
+}
+
+function processDivChange(){
+	if (xhr.readyState < 4) //while waiting for response from server
+		document.getElementById('div1').innerHTML = "Loading...";
+	else if (xhr.readyState === 4) { // 4 = response from server completely loaded
+		if (xhr.status > 199 && xhr.status < 300) // http status between 200 to 299 are all successful
+			document.getElementById('div1').innerHTML = xhr.responseText;
+	}
+}
